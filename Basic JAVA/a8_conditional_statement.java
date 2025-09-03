72e19043d756f22da757e8cb0f3b06d4ad2d85a6
SakshiI10 <iamsakshiingole@gmail.com>
2025-09-03 11:20:14 +0530

    file added

--- Basic JAVA/a8_conditional_statement.java
+++ Basic JAVA/a8_conditional_statement.java
@@ -8,15 +8,31 @@ import java.util.*;
 
 public class a8_conditional_statement {
     public static void main(String[] args) {
-        int originalPass = 1234;
+        String originalPass = "sakshi123";
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter your Password: ");
-        int password = sc.nextInt();
+        String password = sc.nextLine();
 
-        if (password == originalPass) { 
-            System.out.print("Your account is open");
+        if (password.equals(originalPass)) {
+            System.out.println("Your account is open");
         } else {
-            System.out.print("Wrong password");
+            System.out.println("Wrong password");
+        }
+
+        
+        System.out.print("Enter first number: ");
+        int num1=sc.nextInt();
+        System.out.print("Enter first number: ");
+        int num2=sc.nextInt();
+        System.out.print("Which operation: ");
+        char op=sc.next().charAt(0);
+        
+        if(op=='+'){
+            int sum=num1+num2;
+            System.out.print(num1 + " + " + num2 + " = " + sum);;
+        } else if (op=='-'){
+            int diff=num1-num2;
+            System.out.print(num1 + " - " + num2 + " = " + diff);;
         }
     }
 }
