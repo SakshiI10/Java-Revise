4d2a667df6de4b3b0676ddcd089e1265796391d4
SakshiI10 <iamsakshiingole@gmail.com>
2025-09-24 20:53:06 +0530

    myServlet folder edited

--- Servlet/myServlet/src/main/java/com/display/add.java
+++ Servlet/myServlet/src/main/java/com/display/add.java
@@ -8,9 +8,17 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 public class add extends HttpServlet{
-	public void doPost(HttpServletRequest req, HttpServletResponse res) 
+	public void service(HttpServletRequest req, HttpServletResponse res) 
 			throws IOException {
+		// 3. To call another servlet by a servlet using forward [refer message.java] 
+		//PrintWriter out=res.getWriter();
+		//out.print("Second Servlet");
+		
+		// 4. To call another servlet by a servlet using Sendredirect [refer message.java]
+		//System.out.println("Second Servlet");
+		int num=Integer.parseInt(req.getParameter("x"));
+		int sq=num*num;
 		PrintWriter out=res.getWriter();
-		out.print("Second Servlet");
+		out.print("Square = "+sq);
 	}
 }
--- Servlet/myServlet/src/main/java/com/display/message.java
+++ Servlet/myServlet/src/main/java/com/display/message.java
@@ -12,17 +12,23 @@ import jakarta.servlet.http.HttpServletResponse;
 
 
 public class message extends HttpServlet {
-	public void doPost(HttpServletRequest req, HttpServletResponse res) 
+	public void service(HttpServletRequest req, HttpServletResponse res) 
 			throws IOException, ServletException {
+		// 1. For diplaying Hello
 		//String n=req.getParameter("name");
 		//PrintWriter out=res.getWriter();
 		//out.print("Hello "+n);
 		
+		// 2. For displaying addition of 2 numbers
 		int n1=Integer.parseInt(req.getParameter("num1"));
 		int n2=Integer.parseInt(req.getParameter("num2"));
 		int sum=n1+n2;
 		
-		RequestDispatcher rd=req.getRequestDispatcher("add");
-		rd.forward(req, res);
+		// 3. To call another servlet by a servlet using forward [It needs post and doPost]
+		//RequestDispatcher rd=req.getRequestDispatcher("add");
+		//rd.forward(req, res);
+		
+		// 4. To call another servlet by a servlet using Sendredirect [It needs Get and service]
+		res.sendRedirect("add?x="+sum);
 	}
 }
